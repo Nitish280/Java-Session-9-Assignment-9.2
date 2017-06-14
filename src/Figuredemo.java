f3d8000d082df65fce9489015f6a649216da1afd
Nitish280 <nsrccs@gmail.com>
2017-06-14 23:53:35 +0530

    Create Figuredemo.java

--- src/Figuredemo.java
+++ src/Figuredemo.java
@@ -1,9 +1,11 @@
 
 public class Figuredemo {
 	public static void main(String args[]) {
+		/* creating object */
 		Figure crc=new Circle();
 		Figure rct=new Rectangle();
 		Figure tri=new Traingle();
+		/* calling method */
 		crc.area();
 		crc.perimeter();
 		rct.area();
